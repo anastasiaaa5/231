eb1b1d35a68fe6058e16755ee061339629f7b296
anastasiaaa5 <anastasiayunusova@yandex.ru>
2025-01-16 04:12:11 +0000

    Thu, Jan 16, 2025, 11:12 AM +07:00

--- flights/views.py
+++ flights/views.py
@@ -9,5 +9,5 @@ def flight(request, flight_id):
  flight = Flight.objects.get(id=flight_id)
  passengers = flight.passengers.all()
  return render(request, "flights/flight.html",
-              {"flight":flight
+              {"flight":flight,
                "passengers": passengers})
